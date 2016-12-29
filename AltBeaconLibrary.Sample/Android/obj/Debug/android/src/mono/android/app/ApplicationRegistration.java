062e73e9fd8bdaf56946cbadb4aaa065d4b8f630
ron chandra <ronchandra87@gmail.com>
2016-12-29 08:30:34 +0530

    Android Beacon Test
    Android Beacon Test

---
+++ AltBeaconLibrary.Sample/Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("AltBeaconLibrary.Sample.Droid.AltBeaconSampleApplication, AltBeaconLibrary.Sample.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md5fe50bc6a3c709e78e4471611271cfeab.AltBeaconSampleApplication.class, md5fe50bc6a3c709e78e4471611271cfeab.AltBeaconSampleApplication.__md_methods);
+		
+	}
+}
